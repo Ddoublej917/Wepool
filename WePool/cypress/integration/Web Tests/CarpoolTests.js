2724ca42ac328a9c16023c07de4b1de1f3525e7c
Ddoublej917 <djrobotics@ufl.edu>
2022-04-20 15:39:47 -0400

    Updated Cypress commands for new functionality

--- WePool/cypress/integration/Web Tests/CarpoolTests.js
+++ WePool/cypress/integration/Web Tests/CarpoolTests.js
@@ -1,21 +1,30 @@
 describe('Test group info component', () => {
     it('Opens first rider\'s tab', () => {
+      cy.visit('http://localhost:4200/login')
+      cy.contains('Login').click()
+      cy.get('#mat-input-0').type('Danny123')
+      cy.get('#mat-input-1').type('Password456')
+      cy.contains('Sign In').click()
+      cy.saveLocalStorage()
       cy.visit('http://localhost:4200/carpools')
       cy.contains('Rider 1:').click()
       cy.contains('Name:')
       cy.wait(100)
     })
     it('Opens second rider\'s tab', () => {
+      cy.restoreLocalStorage()
       cy.contains('Rider 2:').click()
       cy.contains('Name:')
       cy.wait(100)
     })
     it('Opens third rider\'s tab', () => {
+      cy.restoreLocalStorage()
       cy.contains('Rider 3:').click()
       cy.contains('Name:')
       cy.wait(100)
     })
     it('Opens fourth rider\'s tab', () => {
+      cy.restoreLocalStorage()
       cy.contains('Rider 4:').click()
       cy.contains('Name:')
       cy.wait(100)
